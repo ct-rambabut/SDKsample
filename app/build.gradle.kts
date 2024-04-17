65c70b6b65bc9f2e7e678827d2ddc6fc51ab24ba
RambabuT <rambabu.tekumudi@cartrade.com>
2024-04-17 18:11:40 +0530

    version change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+
 }
 
 dependencies {
--- mylibrarytoast/build.gradle.kts
+++ mylibrarytoast/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.library")
+    id("maven-publish")
     id("org.jetbrains.kotlin.android")
 }
 
@@ -38,3 +39,4 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
